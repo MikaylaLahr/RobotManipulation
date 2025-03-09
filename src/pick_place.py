cf570e5fd7bd460a3c80f46cec3f6804a849e076
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-03-09 15:27:20 -0400

    Improved detection

--- src/pick_place.py
+++ src/pick_place.py
@@ -17,31 +17,27 @@ def main():
     # Initialize the arm module along with the pointcloud and armtag modules
     bot = InterbotixManipulatorXS("wx250s", moving_time=1.5, accel_time=0.75)
     pcl = InterbotixPointCloudInterface()
-    armtag = InterbotixArmTagInterface()
 
-    # # set initial arm and gripper pose
-    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    # bot.gripper.open()
+    # set initial arm and gripper pose
+    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+    bot.gripper.open()
 
-    # # get the ArmTag pose
-    # bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
-    # time.sleep(0.5)
-    armtag.find_ref_to_arm_base_transform()
-    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+    # get the cluster positions
+    # sort them from max to min 'x' position w.r.t. the 'wx250s/base_link' frame
+    success = False
+    while not success:
+        success, clusters = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
+        rospy.loginfo((success, clusters))
 
-    # # get the cluster positions
-    # # sort them from max to min 'x' position w.r.t. the 'wx250s/base_link' frame
-    success, clusters = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
-
-    # # pick up all the objects and drop them in a virtual basket in front of the robot
-    # for cluster in clusters:
-    #     x, y, z = cluster["position"]
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-    #     bot.gripper.close()
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    #     bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    #     bot.gripper.open()
+    # pick up all the objects and drop them in a virtual basket in front of the robot
+    for cluster in clusters:
+        x, y, z = cluster["position"]
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+        bot.gripper.close()
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+        bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        bot.gripper.open()
     bot.arm.go_to_sleep_pose()
 
 if __name__=='__main__':
