5647a13fd10b978c47f0abef54a876ca207604bf
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-04-28 22:53:34 -0400

    cooooooked

--- src/object_reg.cpp
+++ src/object_reg.cpp
@@ -1,47 +1,65 @@
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <open3d/Open3D.h>
 #include <pcl/PCLPointCloud2.h>
+#include <pcl/common/centroid.h>
 #include <pcl/conversions.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/shot_omp.h>
 #include <pcl/filters/filter_indices.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/point_types_conversion.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/transforms.h>
+#include <pcl/correspondence.h>
+#include <pcl/features/board.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/recognition/cg/geometric_consistency.h>
+#include <pcl/recognition/cg/hough_3d.h>
 #include <ros/ros.h>
+#include <rviz_visual_tools/rviz_visual_tools.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
+#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <geometry_msgs/Pose.h>
-#include <vision_msgs/Detection3DArray.h>
-#include <vision_msgs/Detection3D.h>
 #include <vision_msgs/BoundingBox3D.h>
-#include <rviz_visual_tools/rviz_visual_tools.h>
-#include <tf2_eigen/tf2_eigen.h>
+#include <vision_msgs/Detection3D.h>
+#include <vision_msgs/Detection3DArray.h>
+
+#include <boost/filesystem.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <cassert>
 #include <cmath>
 #include <cstddef>
+#include <optional>
 #include <random>
 #include <string>
-#include <optional>
 #include <unordered_map>
 #include <vector>
-#include <boost/filesystem.hpp>
+
 #include "geometry_msgs/Vector3.h"
 #include "gs_matching.h"
 #include "ros/duration.h"
 #include "ros/time.h"
 #include "std_msgs/ColorRGBA.h"
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/io/vtk_lib_io.h>
-#include <pcl/point_types_conversion.h>
-#include <pcl/search/kdtree.h>
-#include <pcl/common/centroid.h>
-#include <pcl/segmentation/conditional_euclidean_clustering.h>
 
 enum ObjectType { Cube, Milk, Wine, Eggs, ToiletPaper, Can, None };
 
 struct ObjectTypeInfo {
     std::string mesh_path;
-    pcl::PointCloud<pcl::PointXYZ> point_cloud;
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr point_cloud;
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr keypoints;
+    pcl::PointCloud<pcl::Normal>::ConstPtr normals;
+    pcl::PointCloud<pcl::SHOT352>::ConstPtr descriptors;
 };
 
 struct Object {
@@ -55,13 +73,9 @@ struct ClusterTrack {
     int id;
     bool active;
     ros::Time last_detected;
-    pcl::PointCloud<pcl::PointXYZHSV> point_cloud;
+    pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr point_cloud;
     pcl::PointXYZHSV mean;
     Object object;
-
-    Eigen::Vector3d cluster_center() const {
-        return Eigen::Vector3d(mean.x, mean.y, mean.z);
-    }
 };
 
 class ObjectRegistration {
@@ -105,6 +119,7 @@ private:
         auto cloud_ptr = boost::make_shared<pcl::PointCloud<pcl::PointXYZHSV>>(hsv);
         auto clusters = cluster_cloud(cloud_ptr);
         update_tracks(std::move(clusters), cloud_msg->header.stamp);
+        update_objects();
         draw_tracks();
 
         visual_tools_->trigger();
@@ -123,15 +138,50 @@ private:
             // {"can", ObjectType::Can}
         };
 
+        open3d::io::ReadTriangleMeshOptions options;
+        options.enable_post_processing = false;
+        options.print_progress = false;
+
+        pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> norm_est;
+        norm_est.setKSearch(10);
+
+        pcl::SHOTEstimationOMP<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> descr_est;
+        descr_est.setRadiusSearch(0.02);
+
+        pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
+
         for (const auto& entry:
             boost::make_iterator_range(boost::filesystem::directory_iterator(mesh_folder), {})) {
-            
-            pcl::PolygonMesh mesh;
-            pcl::io::loadPolygonFileSTL(entry.path().string(), mesh);
-            pcl::PointCloud<pcl::PointXYZ> cloud;
-            pcl::fromPCLPointCloud2(mesh.cloud, cloud);
+            open3d::geometry::TriangleMesh mesh;
+            open3d::io::ReadTriangleMesh(entry.path().string(), mesh, options);
 
-            ObjectTypeInfo data{entry.path().string(), cloud};
+            double surface_area = mesh.GetSurfaceArea();  // m^2
+            double points_per_m_2 = 100000;               // based on voxel size?
+            auto point_cloud = *mesh.SamplePointsUniformly(surface_area * points_per_m_2, false);
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+            for (const auto& o3d_point: point_cloud.points_) {
+                pcl::PointXYZ pcl_point;
+                pcl_point.getVector3fMap() = o3d_point.cast<float>();
+                pcl_cloud->push_back(pcl_point);
+            }
+
+            pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>());
+            norm_est.setInputCloud(pcl_cloud);
+            norm_est.compute(*normals);
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints(new pcl::PointCloud<pcl::PointXYZ>());
+            uniform_sampling.setInputCloud(pcl_cloud);
+            uniform_sampling.setRadiusSearch(0.03);
+            uniform_sampling.filter(*keypoints);
+
+            pcl::PointCloud<pcl::SHOT352>::Ptr descriptors(new pcl::PointCloud<pcl::SHOT352>());
+            descr_est.setInputCloud(keypoints);
+            descr_est.setInputNormals(normals);
+            descr_est.setSearchSurface(pcl_cloud);
+            descr_est.compute(*descriptors);
+
+            ObjectTypeInfo data{entry.path().string(), pcl_cloud, keypoints, normals, descriptors};
 
             std::string filename = entry.path().filename().replace_extension("").string();
             if (name_type_map.find(filename) != name_type_map.end()) {
@@ -169,7 +219,7 @@ private:
             visual_tools_->publishSphere(center, rgba, scale, "sphere", track.id + 1);
             visual_tools_->publishText(text_pose, std::to_string(track.id),
                 rviz_visual_tools::WHITE, rviz_visual_tools::MEDIUM, false);
-            
+
             if (track.object.type == ObjectType::None) {
                 continue;
             }
@@ -182,7 +232,8 @@ private:
         }
     }
 
-    // vision_msgs::Detection3DArray convert_objects_to_ros(std_msgs::Header header) {
+    // vision_msgs::Detection3DArray convert_objects_to_ros(std_msgs::Header
+    // header) {
     //     vision_msgs::Detection3DArray detection_array;
     //     detection_array.header = header;
 
@@ -221,7 +272,6 @@ private:
 
     void update_tracks(
         std::vector<pcl::PointCloud<pcl::PointXYZHSV>> clusters, ros::Time detection_time) {
-
         std::vector<pcl::PointXYZHSV> hsv_means(clusters.size());
         for (size_t i = 0; i < clusters.size(); i++) {
             hsv_means[i] = compute_cluster_mean(clusters[i]);
@@ -229,9 +279,9 @@ private:
 
         std::vector<int> track_matches(tracks_.size(), -1);
         std::vector<bool> clusters_taken(clusters.size(), false);
-        
+
         for (size_t j = 0; j < tracks_.size(); j++) {
-            const auto &track = tracks_[j];
+            const auto& track = tracks_[j];
             Eigen::Vector3d track_center(track.mean.x, track.mean.y, track.mean.z);
 
             int best_cluster = -1;
@@ -264,25 +314,28 @@ private:
                     continue;
                 }
 
-                Eigen::Vector3d other_cluster_center(hsv_means[best_cluster].x, hsv_means[best_cluster].y, hsv_means[best_cluster].z);
-                if ((track_center - cluster_center).norm() < (track_center - other_cluster_center).norm()) {
+                Eigen::Vector3d other_cluster_center(hsv_means[best_cluster].x,
+                    hsv_means[best_cluster].y, hsv_means[best_cluster].z);
+                if ((track_center - cluster_center).norm()
+                    < (track_center - other_cluster_center).norm()) {
                     best_cluster = i;
                     clusters_taken[i] = true;
-                }   
+                }
             }
 
             track_matches[j] = best_cluster;
         }
 
         for (size_t i = 0; i < track_matches.size(); i++) {
-            auto &track = tracks_[i];
+            auto& track = tracks_[i];
 
             if (track_matches[i] != -1) {
-                const auto &cluster = clusters[track_matches[i]];
+                const auto& cluster = clusters[track_matches[i]];
                 track.active = true;
                 track.last_detected = detection_time;
                 track.mean = compute_cluster_mean(cluster);
-                track.point_cloud = std::move(cluster);
+                track.point_cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZHSV>>(
+                    std::move(cluster));
             } else {
                 track.active = false;
             }
@@ -295,9 +348,11 @@ private:
                 track.active = true;
                 track.last_detected = detection_time;
                 track.mean = compute_cluster_mean(clusters[i]);
-                track.point_cloud = std::move(clusters[i]);
+                track.point_cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZHSV>>(
+                    std::move(clusters[i]));
                 track.object.type = ObjectType::None;
-                track.object.redetection_time = ros::Time::ZERO;
+                track.object.redetection_time = generate_future_time(
+                    std::normal_distribution<double>(1.0, 0.5));
                 tracks_.push_back(track);
             }
         }
@@ -309,21 +364,21 @@ private:
             if (!track.active && detection_time - track.last_detected > ros::Duration(1)) {
                 continue;
             }
-            
+
             recent_tracks.push_back(track);
         }
 
         tracks_ = std::move(recent_tracks);
     }
 
-    pcl::PointXYZHSV compute_cluster_mean(const pcl::PointCloud<pcl::PointXYZHSV> &cluster) {
+    pcl::PointXYZHSV compute_cluster_mean(const pcl::PointCloud<pcl::PointXYZHSV>& cluster) {
         Eigen::Vector3f centroid = Eigen::Vector3f::Zero();
         Eigen::Vector2f hue_avg = Eigen::Vector2f::Zero();
         float sat_avg = 0;
         float val_avg = 0;
         size_t num_hue_avg = 0;
 
-        for (const auto &point: cluster) {
+        for (const auto& point: cluster) {
             centroid += point.getVector3fMap();
 
             if (point.s > 0.2) {
@@ -357,37 +412,158 @@ private:
         return mean;
     }
 
-    // void update_objects() {
-    //     for (auto &track: tracks_) {
-    //         if (!track.active) {
-    //             continue;
-    //         }
+    void update_objects() {
+        double fitness_threshold = -0.1;
 
-    //         if (ros::Time::now() > track.object.redetection_time) {
-    //             // detect
-    //         } else {
-    //             if (track.object.type == ObjectType::None) {
-    //                 continue;
-    //             }
+        for (auto& track: tracks_) {
+            if (!track.active) {
+                continue;
+            }
 
-    //             // track
-    //         }
-    //     }
-    // }
+            if (ros::Time::now() > track.object.redetection_time) {
+                const auto& [type, pose, fitness] = detect_object_from_scratch(track.point_cloud);
 
-    std::tuple<ObjectType, Eigen::Isometry3d, double> detect_object_from_scratch(
-        pcl::PointCloud<pcl::PointXYZRGB>& cluster) {
-        
+                if (fitness > fitness_threshold) {
+                    track.object.type = type;
+                    track.object.pose = pose;
+                    track.object.redetection_time = generate_future_time(
+                        std::normal_distribution<double>(5.0, 2.0));
+                } else {
+                    track.object.type = ObjectType::None;
+                    track.object.redetection_time = generate_future_time(
+                        std::normal_distribution<double>(10.0, 5.0));
+                }
+            } else {
+                if (track.object.type == ObjectType::None) {
+                    continue;
+                }
+
+                // track
+            }
+        }
     }
 
-    std::vector<pcl::PointCloud<pcl::PointXYZHSV>> cluster_cloud(pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr cloud) {
+    std::tuple<ObjectType, Eigen::Isometry3d, double> detect_object_from_scratch(
+        const pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr cluster) {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr xyz_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+        xyz_cluster->reserve(cluster->size());
+        for (const auto& point: cluster->points) {
+            xyz_cluster->emplace_back(point.x, point.y, point.z);
+        }
+
+        const ObjectTypeInfo& wine = object_types_[ObjectType::Wine];
+
+        pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> norm_est;
+        norm_est.setKSearch(10);
+
+        pcl::SHOTEstimationOMP<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> descr_est;
+        descr_est.setRadiusSearch(0.02);
+
+        pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
+
+        pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>());
+        norm_est.setInputCloud(xyz_cluster);
+        norm_est.compute(*normals);
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints(new pcl::PointCloud<pcl::PointXYZ>());
+        uniform_sampling.setInputCloud(xyz_cluster);
+        uniform_sampling.setRadiusSearch(0.03);
+        uniform_sampling.filter(*keypoints);
+
+        pcl::PointCloud<pcl::SHOT352>::Ptr descriptors(new pcl::PointCloud<pcl::SHOT352>());
+        descr_est.setInputCloud(keypoints);
+        descr_est.setInputNormals(normals);
+        descr_est.setSearchSurface(xyz_cluster);
+        descr_est.compute(*descriptors);
+
+        //
+        //  Find Model-Scene Correspondences with KdTree
+        //
+        pcl::CorrespondencesPtr corrs(new pcl::Correspondences());
+
+        pcl::KdTreeFLANN<pcl::SHOT352> match_search;
+        match_search.setInputCloud(wine.descriptors);
+
+        //  For each scene keypoint descriptor, find nearest neighbor into the
+        //  model keypoints descriptor cloud and add it to the correspondences
+        //  vector.
+        for (std::size_t i = 0; i < descriptors->size(); ++i) {
+            std::vector<int> neigh_indices(1);
+            std::vector<float> neigh_sqr_dists(1);
+            if (!std::isfinite(descriptors->at(i).descriptor[0]))  // skipping NaNs
+            {
+                continue;
+            }
+
+            int found_neighs = match_search.nearestKSearch(
+                descriptors->at(i), 1, neigh_indices, neigh_sqr_dists);
+            if (found_neighs == 1 && neigh_sqr_dists[0] < 0.25f) {
+                pcl::Correspondence corr(neigh_indices[0], static_cast<int>(i), neigh_sqr_dists[0]);
+                corrs->push_back(corr);
+            }
+        }
+
+        //
+        //  Actual Clustering
+        //
+        std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> rototranslations;
+        std::vector<pcl::Correspondences> clustered_corrs;
+
+        pcl::PointCloud<pcl::ReferenceFrame>::Ptr model_rf(
+            new pcl::PointCloud<pcl::ReferenceFrame>());
+        pcl::PointCloud<pcl::ReferenceFrame>::Ptr scene_rf(
+            new pcl::PointCloud<pcl::ReferenceFrame>());
+
+        pcl::BOARDLocalReferenceFrameEstimation<pcl::PointXYZ, pcl::Normal, pcl::ReferenceFrame>
+            rf_est;
+        rf_est.setFindHoles(true);
+        rf_est.setRadiusSearch(0.015);
+
+        rf_est.setInputCloud(wine.keypoints);
+        rf_est.setInputNormals(wine.normals);
+        rf_est.setSearchSurface(wine.point_cloud);
+        rf_est.compute(*model_rf);
+
+        rf_est.setInputCloud(keypoints);
+        rf_est.setInputNormals(normals);
+        rf_est.setSearchSurface(xyz_cluster);
+        rf_est.compute(*scene_rf);
+
+        //  Clustering
+        pcl::Hough3DGrouping<pcl::PointXYZ, pcl::PointXYZ, pcl::ReferenceFrame, pcl::ReferenceFrame>
+            clusterer;
+        clusterer.setHoughBinSize(0.01);
+        clusterer.setHoughThreshold(5.0);
+        clusterer.setUseInterpolation(true);
+        clusterer.setUseDistanceWeight(false);
+
+        clusterer.setInputCloud(wine.keypoints);
+        clusterer.setInputRf(model_rf);
+        clusterer.setSceneCloud(keypoints);
+        clusterer.setSceneRf(scene_rf);
+        clusterer.setModelSceneCorrespondences(corrs);
+
+        clusterer.recognize(rototranslations, clustered_corrs);
+
+        Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
+        if (!rototranslations.empty()) {
+            ROS_WARN("No objects found!");
+            pose = Eigen::Isometry3d(rototranslations.front().cast<double>());
+        }
+
+        return {ObjectType::Wine, pose, 1.0};
+    }
+
+    std::vector<pcl::PointCloud<pcl::PointXYZHSV>> cluster_cloud(
+        pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr cloud) {
         pcl::ConditionalEuclideanClustering<pcl::PointXYZHSV> reg;
         pcl::search::Search<pcl::PointXYZHSV>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZHSV>);
         pcl::IndicesPtr indices(new std::vector<int>);
 
         pcl::removeNaNFromPointCloud(*cloud, *indices);
 
-        auto condition_func = [](const pcl::PointXYZHSV &first, const pcl::PointXYZHSV &second, float squared_dist) {
+        auto condition_func = [](const pcl::PointXYZHSV& first, const pcl::PointXYZHSV& second,
+                                  float squared_dist) {
             double hue_dist = 0.0;
             if (std::abs(second.h - first.h) > 180) {
                 hue_dist = 360 - std::abs(second.h - first.h);
@@ -402,7 +578,7 @@ private:
             if (first.s > 0.1 && second.s > 0.1 && first.v > 0.1 && second.v > 0.1) {
                 return hue_dist < 5 && sat_dist < 0.05 && val_dist < 0.05;
             } else {
-                return sat_dist < 0.1 && val_dist < 0.1;
+                return sat_dist < 0.05 && val_dist < 0.05;
             }
         };
 
@@ -412,14 +588,14 @@ private:
         reg.setConditionFunction(std::function(condition_func));
         reg.setMinClusterSize(200);
         reg.setClusterTolerance(0.02);
-      
+
         std::vector<pcl::PointIndices> point_indices;
         reg.segment(point_indices);
 
         std::vector<pcl::PointCloud<pcl::PointXYZHSV>> clusters(point_indices.size());
         for (size_t i = 0; i < point_indices.size(); i++) {
             clusters[i].reserve(point_indices[i].indices.size());
-            for (auto point_idx : point_indices[i].indices) {
+            for (auto point_idx: point_indices[i].indices) {
                 clusters[i].push_back(cloud->points[point_idx]);
             }
         }
