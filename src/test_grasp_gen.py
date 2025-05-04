f6f50f0b97e533c8b15a93ff89cf7fa7e3f5ed67
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-05-03 22:03:13 -0400

    taking a break from this

--- src/test_grasp_gen.py
+++ src/test_grasp_gen.py
@@ -7,13 +7,42 @@ from sensor_msgs.msg import PointCloud2, PointField
 from robot_manipulation.srv import GenerateGraspCandidates, GenerateGraspCandidatesRequest, GenerateGraspCandidatesResponse
 import struct
 from numpy_ros import to_numpy, to_message
-
+import sys
+import copy
+import moveit_commander
+import moveit_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from moveit_commander.conversions import pose_to_list
+import time
+from geometry_msgs.msg import Pose, Point
+from moveit_msgs.msg import RobotState, Constraints, PositionConstraint, BoundingVolume
+from shape_msgs.msg import SolidPrimitive
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 
-OBJECT_POSE_GLOBAL = np.array([[1, 0, 0, 0.276],
+
+OBJECT_POSE_GLOBAL = np.array([[1, 0, 0, 0.3],
                                [0, 1, 0, 0],
-                               [0, 0, 1, 0.0635],
+                               [0, 0, 1, 0.07],
                                [0, 0, 0, 1]])
+
+class MoveItInterface:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.robot = moveit_commander.RobotCommander(robot_description="wx250s/robot_description", ns="wx250s")
+        self.scene = moveit_commander.PlanningSceneInterface(ns="wx250s")
+        self.move_group = moveit_commander.MoveGroupCommander("interbotix_arm", robot_description="wx250s/robot_description", ns="wx250s", wait_for_servers=10.0)
+        self.display_trajectory_publisher = rospy.Publisher("move_group/display_planned_path",
+                                                    moveit_msgs.msg.DisplayTrajectory,
+                                                    queue_size=20)
+    
+    def display_trajectory(self, plan):
+        trajectory = moveit_msgs.msg.DisplayTrajectory()
+        trajectory.trajectory_start = self.robot.get_current_state()
+        trajectory.trajectory.append(plan)
+        self.display_trajectory_publisher.publish(trajectory)
+
 
 # Function to convert Open3D PointCloud to ROS PointCloud2
 def convert_open3d_to_ros_pointcloud2(open3d_cloud, frame_id="base_link"):
@@ -70,11 +99,9 @@ def convert_open3d_to_ros_pointcloud2(open3d_cloud, frame_id="base_link"):
     return ros_cloud
 
 def main():
-    """
-    ROS node to load an STL, convert to PointCloud2, and call a service.
-    """
-
-    bot = InterbotixManipulatorXS("wx250s", moving_time=3.0, accel_time=1.5)
+    # only used for gripper
+    rospy.init_node("test_grasp_gen")
+    moveit = MoveItInterface()
 
     # --- Parameters ---
     # Replace with the actual path to your STL file
@@ -133,26 +160,79 @@ def main():
     # If using a request object:
     response: GenerateGraspCandidatesResponse = service_proxy(request) # Replace with your actual service call
 
+    # bot.gripper.open()
+
+    constraints = Constraints()
+    position_constraint = PositionConstraint()
+    bounding_volume = BoundingVolume()
+    box = SolidPrimitive()
+    box.type = SolidPrimitive.BOX
+    box.dimensions = [1.0, 0.6, 0.7]
+    box_pose = Pose()
+    box_pose.position.x = 0.25
+    box_pose.position.y = 0
+    box_pose.position.z = 0.38
+    bounding_volume.primitives = [box]
+    bounding_volume.primitive_poses = [box_pose]
+    position_constraint.link_name = "wx250s/ee_gripper_link"
+    position_constraint.constraint_region = bounding_volume
+    position_constraint.target_point_offset.x = 0
+    position_constraint.target_point_offset.y = 0
+    position_constraint.target_point_offset.z = 0
+    position_constraint.weight = 1.0
+    position_constraint.header.frame_id = 'wx250s/base_link'
+    constraints.name = "workspace"
+    constraints.position_constraints = [position_constraint]
+    moveit.move_group.set_path_constraints(constraints)
+
     for candidate in sorted(response.candidates.candidates, key=lambda x: x.score, reverse=True):
         approach = to_numpy(candidate.approach)
-        gripper_rot = np.column_stack([approach, to_numpy(candidate.binormal), to_numpy(candidate.axis)])
+        binormal = to_numpy(candidate.binormal)
+        axis = to_numpy(candidate.axis)
+
+        gripper_rot = np.column_stack([approach, binormal, axis])
         gripper_t = to_numpy(candidate.position)
         pose = np.block([[gripper_rot, gripper_t.reshape((-1, 1))], [np.zeros(3), 1]])
         pose_global = OBJECT_POSE_GLOBAL @ pose
-        pose_approach = OBJECT_POSE_GLOBAL @ np.block([[np.eye(3), (0.02 * approach).reshape((-1, 1))], [np.zeros(3), 1]]) @ pose
+        pose_approach = OBJECT_POSE_GLOBAL @ np.block([[np.eye(3), (-0.05 * approach).reshape((-1, 1))], [np.zeros(3), 1]]) @ pose
 
-        approach_theta_list, feasible_approach = bot.arm.set_ee_pose_matrix(pose_global, execute=False)
-        guess = None if not feasible_approach else approach_theta_list
-        final_theta_list, feasible_final = bot.arm.set_ee_pose_matrix(pose_approach, execute=False, custom_guess=guess)
-        thetas_close = np.linalg.norm(approach_theta_list - final_theta_list) < 0.2
+        pose_approach_ros = to_message(Pose, pose_approach)
+        pose_global_ros = to_message(Pose, pose_global)
+        waypoints = [pose_approach_ros, pose_global_ros]
 
-        if feasible_approach and feasible_final and thetas_close:
-            print("found feasible grasp")
-            bot.gripper.open()
-            bot.arm.set_joint_positions(approach_theta_list)
-            bot.arm.set_joint_positions(final_theta_list)
-            bot.gripper.close()
+        success_approach, approach_plan, plan_time, code = moveit.move_group.plan(pose_approach_ros)
+        if not success_approach:
+            continue
+
+        traj_point = approach_plan.joint_trajectory.points[len(approach_plan.joint_trajectory.points) - 1]
+        joint_state = JointState()
+        joint_state.header = approach_plan.joint_trajectory.header
+        joint_state.name = approach_plan.joint_trajectory.joint_names
+        joint_state.position = traj_point.positions
+        joint_state.velocity = traj_point.velocities
+        joint_state.effort = traj_point.effort
+        moveit_robot_state = RobotState()
+        moveit_robot_state.joint_state = joint_state
+        moveit.move_group.set_start_state(moveit_robot_state)
+        success_final, final_plan, plan_time, code = moveit.move_group.plan(pose_global_ros)
+        if not success_final:
+            continue
+
+        moveit.move_group.execute(approach_plan)
+        moveit.move_group.stop()
+
+        moveit.move_group.set_start_state(moveit.robot.get_current_state())
+        success_final, final_plan, plan_time, code = moveit.move_group.plan(pose_global_ros)
+        if not success_final:
+            print("doom")
             break
+
+        moveit.move_group.execute(final_plan)
+        moveit.move_group.stop()
+        # bot.gripper.close()
+
+        break
+
 
 
 if __name__ == '__main__':
